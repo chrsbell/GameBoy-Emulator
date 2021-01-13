92e07a3eef287614a2a8f4af13e5d519e58ceabe
Chris Bell <cb.camellia@gmail.com>
2021-01-13 12:28:04 -0800

    Add primitive type classes

--- client/src/components/Memory/Memory.ts
+++ client/src/components/Memory/Memory.ts
@@ -1,22 +1,43 @@
-import type { Hex } from '../Types';
+import { add } from 'lodash';
+import { Byte, ByteArray } from '../Types';
 
-const ROMSizeCodes = {
-  0x00: { size: '32kB', banks: 2 },
-  0x01: { size: '64kB', banks: 4 },
-  0x02: { size: '128kB', banks: 8 },
-  0x03: { size: '256kB', banks: 16 },
-  0x04: { size: '512kB', banks: 32 },
-  0x05: { size: '1MB', banks: 64 },
+interface CartridgeCode {
+  [key: number]: string;
+}
+
+interface ROMCode {
+  [key: number]: { size: string; numBanks: number };
+}
+
+const ROMSizeCodes: ROMCode = {
+  0x00: { size: '32kB', numBanks: 2 },
+  0x01: { size: '64kB', numBanks: 4 },
+  0x02: { size: '128kB', numBanks: 8 },
+  0x03: { size: '256kB', numBanks: 16 },
+  0x04: { size: '512kB', numBanks: 32 },
+  0x05: { size: '1MB', numBanks: 64 },
 };
 
-const CartridgeTypes = {
+const CartridgeTypes: CartridgeCode = {
   0x00: 'ROM Only',
   0x01: 'MBC #1',
-  0x02: 'MBC #1 + external RAM',
-  0x03: 'MBC #1 + external RAM + battery',
+  0x02: 'MBC #1 + RAM',
+  0x03: 'MBC #1 + RAM + Battery',
+  0x05: 'MBC2',
+  0x06: 'MBC2 + Battery',
+  0x08: 'ROM + RAM',
+  0x09: 'MBCM #1',
+  0x0b: 'MBCM #1 + RAM',
+  0x0c: 'MBCM #1 + RAM + Battery',
+  0x0d: 'MBCM #1 + RAM + Battery',
+  0x0f: 'MBC #3 + Timer + Battery',
+  0x10: 'MBC #3 + Timer + RAM + Battery',
+  0x11: 'MBC #3',
+  0x12: 'MBC #3 + RAM',
+  0x13: 'MBC #3 + RAM + Battery',
 };
 
-const RAMSizeCodes = {
+const RAMSizeCodes: CartridgeCode = {
   0x00: 'None',
   0x01: '2kB',
   0x02: '8kB',
@@ -27,116 +48,162 @@ const RAMSizeCodes = {
 
 interface Cartridge {
   // entire cartridge ROM
-  ROM: Array<Hex>;
+  ROM: ByteArray;
   // the ROM size code
-  ROMSize: number;
+  ROMSize: Byte;
   // external RAM size code
-  RAMSize: number;
+  RAMSize: Byte;
   // the MBC type code
-  type: number;
+  MBCType: Byte;
   // each ROM Bank is 16kB
-  ROMBanks: Array<Hex>;
+  ROMBanks: Array<ByteArray>;
   // 4 banks for 32kB RAM sizes
   // https://gbdev.io/pandocs/#a000-bfff-ram-bank-00-03-if-any-read-write
-  RAMBanks: Array<Hex>;
+  RAMBanks: Array<ByteArray>;
   R: {
     // enabled with 0Ah in lower 4 bits
     RAMEnabled: boolean;
     // current ROM bank
     // https://gbdev.io/pandocs/#_0000-3fff-rom-bank-00-20-40-60-read-only
-    currROMBank: number;
+    currROMBank: Byte;
     // current RAM bank (1 bank for RAM with 2kB-8kB sizes)
-    currRAMBank: number;
+    currRAMBank: Byte;
     // 2 bit register
-    ROMRAMMixed: number;
-    bankingMode: number;
+    ROMRAMMixed: Byte;
+    bankingMode: Byte;
   };
 }
 
 class Memory {
-  private cartridge: Cartridge;
-  private bios: Array<Hex>;
+  private bios: ByteArray;
+  private cart: Cartridge;
+  // 8k vRAM
+  private vRAM: ByteArray;
+  // 8k internal RAM
+  private wRAM: ByteArray;
+  // shadow of working RAM, (8k - 512) bytes
+  private wRAMShadow: ByteArray;
+  // sprite attribute table
+  private OAM: ByteArray;
+  // 126 bytes high RAM
+  private hRAM: ByteArray;
   private initialized: boolean = false;
   constructor() {
+    this.vRAM = new ByteArray(0x9fff - 0x8000);
+    this.wRAM = new ByteArray(0xdfff - 0xc000);
+    this.wRAMShadow = new ByteArray(0xfdff - 0xe000);
+    this.OAM = new ByteArray(0xfe9f - 0xfe00);
+    this.hRAM = new ByteArray(0xfffe - 0xff80);
     // defaults to bank 1 at power on
-    this.cartridge.R.currROMBank = 1;
+    this.cart = {
+      ROM: null as ByteArray,
+      ROMSize: new Byte(0),
+      RAMSize: new Byte(0),
+      MBCType: new Byte(0),
+      ROMBanks: null as Array<ByteArray>,
+      RAMBanks: null as Array<ByteArray>,
+      R: {
+        RAMEnabled: false,
+        currROMBank: new Byte(1),
+        currRAMBank: new Byte(1),
+        ROMRAMMixed: new Byte(0),
+        bankingMode: new Byte(0),
+      },
+    };
+    this.cart.R.currROMBank = new Byte(1);
   }
 
-  write(address: number, data: number) {
-    if (address < 0x8000) {
+  write(address: Byte, data: Byte) {
+    /*if (address < 0x8000) {
       this.changeBank(address, data);
     } else if (address >= 0xc000 && address <= 0xdfff) {
-      this.cartridge.ROM[address] = data;
+      this.cart.ROM[address] = data;
       // write to echo RAM as well
-      this.cartridge.ROM[address + 0x2000] = data;
+      this.cart.ROM[address + 0x2000] = data;
     } else if (address >= 0xe000 && address <= 0xfdff) {
       throw new Error(`Can't write to prohibited address: ${address.toString(16)}`);
-    }
+    }*/
   }
 
-  changeBank(address: number, data: number) {
-    if (address < 0x2000) {
+  changeBank(address: Byte, data: Byte) {
+    if (address.value() < 0x2000) {
       // RAM enable register
-      this.cartridge.R.RAMEnabled = (data & 0b00001111) === 0xa;
-    } else if (address < 0x4000) {
+      this.cart.R.RAMEnabled = (data.value() & 0b00001111) === 0xa;
+    } else if (address.value() < 0x4000) {
       // ROM Bank change (only the lower 5 bits)
-      this.cartridge.R.currROMBank = data & 0b00011111;
-    } else if (address < 0x6000) {
-      const register = data & 0b11;
-      this.cartridge.R.ROMRAMMixed = register;
+      this.cart.R.currROMBank.set(data.value() & 0b00011111);
+    } else if (address.value() < 0x6000) {
+      const register = data.value() & 0b11;
+      this.cart.R.ROMRAMMixed.set(register);
       // swap to one of the four RAM banks if size is 32kB
-      if (this.cartridge.RAMSize >= 0x03) {
-        this.cartridge.R.currRAMBank = register;
+      if (this.cart.RAMSize.value() >= 0x03) {
+        this.cart.R.currRAMBank.set(register);
       }
-      if (this.cartridge.R.bankingMode === 0 && this.cartridge.ROMSize >= 0x05) {
+      if (this.cart.R.bankingMode.value() === 0 && this.cart.ROMSize.value() >= 0x05) {
         // for 1MB ROM or larger carts, set upper 2 bits (5-6) of ROM bank number
-        this.cartridge.R.currROMBank |= register << 4;
+        const { currROMBank } = this.cart.R;
+        this.cart.R.currROMBank.set(currROMBank.value() | (register << 4));
       }
       // for 1MB ROM multi carts, apply same operation but only to bits 4-5
     } else {
       // Banking mode select
-      this.cartridge.R.bankingMode = data & 0x01;
+      this.cart.R.bankingMode.set(data.value() & 0x01);
     }
   }
   /**
    * Return the value at the address as a number
    */
-  read(address: number) {
-    // ROM Bank 0 is always available
+  read(address: number): number {
     if (address < 0x4000) {
-      return this.cartridge.ROM[address];
-    } else if (address >= 0x4000 && address <= 0x7fff) {
+      // ROM Bank 0 is always available
+      return this.cart.ROM[address];
+    } else if (address <= 0x7fff) {
       // Reading from ROM bank of cartridge
-      // For non-MBC cartridges, assume cartridge ROM is less than 32kB
-      return this.cartridge.ROM[address];
-    } else if (address >= 0xa000 && address <= 0xbfff) {
-      // reading from RAM bank
-      // address -= 0xa000;
-      // return this.cartridge.ROM[address + this.registers.RAMBank * 0x2000];
+      if (this.cart.MBCType.value() === 0) {
+        return this.cart.ROM[address];
+      }
+    } else if (address <= 0x9fff) {
+      return this.vRAM[address - 0x8000];
+    } else if (address <= 0xbfff) {
+      // reading from RAM bank of cartridge
+      return this.cart.RAMBanks[this.cart.R.currRAMBank.value()][address - 0xa000];
+    } else if (address <= 0xdfff) {
+      return this.wRAM[address - 0xc000];
+    } else if (address <= 0xfdff) {
+      return this.wRAMShadow[address - 0xe000];
+    } else if (address <= 0xfe9f) {
+      return this.OAM[address - 0xfe00];
+    } else if (address <= 0xff7f) {
+      // hardware I/O
+    } else if (address <= 0xffff) {
+      return this.hRAM[address - 0xff80];
     }
-    return this.cartridge.ROM[address];
+    return this.cart.ROM[address];
   }
   /**
    * Load a file into ROM
    */
-  loadFile(rom: Array<Hex>) {
-    this.cartridge.ROM = rom;
-    this.cartridge.type = this.read(0x147);
-    this.cartridge.ROMSize = this.read(0x148);
-    this.cartridge.RAMSize = this.read(0x149);
-    console.log(`ROM Size: $${this.cartridge.ROMSize}`);
-    console.log(`RAM Size: ${this.cartridge.RAMSize}`);
-    if (!this.cartridge.type) {
+  loadFile(rom: ByteArray) {
+    this.cart.ROM = rom;
+    this.cart.MBCType = new Byte(this.read(0x147));
+    this.cart.ROMSize = new Byte(this.read(0x148));
+    this.cart.RAMSize = new Byte(this.read(0x149));
+    console.log(`ROM Size: $${JSON.stringify(ROMSizeCodes[this.cart.ROMSize.value()])}`);
+    console.log(`RAM Size: ${RAMSizeCodes[this.cart.RAMSize.value()]}`);
+    console.log(`Cartridge Type: ${CartridgeTypes[this.cart.MBCType.value()]}`);
+
+    if (this.cart.MBCType.value() === 0) {
+      // MBC 0x00
       this.initialized = true;
     } else {
-      console.log(`No support for MBC ${this.cartridge.type.toString(16)}.`);
+      console.log(`No support for MBC ${this.cart.MBCType.log()}.`);
     }
     console.log('Loaded file into ROM memory.');
   }
   /**
    * Load a file into BIOS
    */
-  loadBios(bios: Array<Hex>) {
+  loadBios(bios: ByteArray) {
     this.bios = bios;
     console.log('Loaded bios.');
   }
--- client/src/components/Types.ts
+++ client/src/components/Types.ts
@@ -1,1 +1,42 @@
-export type Hex = number;
+const pad = function (str: string, width: number, replacement: string = '0'): string {
+  if (str.length >= width) {
+    return str;
+  }
+  return Array(width - str.length).join(replacement) + str;
+};
+
+interface Primitive {
+  log: () => string;
+}
+
+export class Byte extends Uint8Array implements Primitive {
+  constructor(value: number) {
+    super([value]);
+  }
+  value(): number {
+    return this[0];
+  }
+  set<T>(value: T) {
+    this[0] = Number(value);
+  }
+  log(): string {
+    return `0x${pad(this[0].toString(16), 2)}`;
+  }
+}
+
+export class ByteArray extends Uint8Array {}
+
+export class Word extends Uint16Array implements Primitive {
+  constructor(value: number) {
+    super([value]);
+  }
+  value(): number {
+    return this[0];
+  }
+  set<T>(value: T) {
+    this[0] = Number(value);
+  }
+  log(): string {
+    return `0x${pad(this[0].toString(16), 2)}`;
+  }
+}
